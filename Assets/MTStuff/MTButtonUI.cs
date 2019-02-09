1dbcfed2a29cb64fd1d31ef2c9bda7e3375f577b
matthewtinker22 <38359672+matthewtinker22@users.noreply.github.com>
2019-02-09 14:56:23 -0500

    ButtonScript
    Started working on a button script, but didn't really do anything worthwhile. I just created the ButtonScript and added button variables inside, however, they don't do anything right now.

---
+++ Assets/MTStuff/MTButtonUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonUI : MonoBehaviour
+{
+    Button magicButton;
+    Button attackButton;
+    Button fireAttack;
+    Button iceAttack;
+    Button thunderAttack;
+    Button healButton;
+    Button backButton;
+
+    // Use this for initialization
+    void Start ()
+    {
+        magicButton = GetComponent<Button>();
+        attackButton = GetComponent<Button>();
+        fireAttack = GetComponent<Button>();
+        iceAttack = GetComponent<Button>();
+        thunderAttack = GetComponent<Button>();
+        healButton = GetComponent<Button>();
+        backButton = GetComponent<Button>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
