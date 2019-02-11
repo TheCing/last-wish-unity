d3f9bcf475ac6bec24aa962aab117b621efdcde1
matthewtinker22 <38359672+matthewtinker22@users.noreply.github.com>
2019-02-11 14:13:15 -0500

    Edited Button Script
    Edited the Button Script, but they still don't work or do anything yet

---
+++ Assets/MTStuff/MTButtonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MTButtonController : MonoBehaviour
+{
+    public Button attackButton;
+    public Button magicButton;
+    public Button fireAttack;
+    public Button iceAttack;
+    public Button thunderAttack;
+    public Button healButton;
+    public Button backButton;
+
+	// Use this for initialization
+	void Start ()
+    {
+        attackButton = GameObject.Find("AttackButton").GetComponent<Button>();
+        magicButton = GameObject.Find("MagicButton").GetComponent<Button>();
+        fireAttack = GameObject.Find("FireAttack").GetComponent<Button>();
+        iceAttack = GameObject.Find("IceAttack").GetComponent<Button>();
+        thunderAttack = GameObject.Find("ThunderAttack").GetComponent<Button>();
+        healButton = GameObject.Find("Heal").GetComponent<Button>();
+        backButton = GameObject.Find("Back").GetComponent<Button>();
+    }
+	
+	void HideButton()
+    {
+        fireAttack.gameObject.SetActive(false);
+        iceAttack.gameObject.SetActive(false);
+        thunderAttack.gameObject.SetActive(false);
+        healButton.gameObject.SetActive(false);
+    }
+}
