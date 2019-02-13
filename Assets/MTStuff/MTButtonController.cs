63079facabd4f60087e18bbeb5826c135470c402
matthewtinker22 <38359672+matthewtinker22@users.noreply.github.com>
2019-02-13 13:18:41 -0500

    Got the Buttons to work
    The buttons now display. If the user clicks magic, then the magic options display. Once they click the magic attack they want to use, or hit back, the magic options are hidden once again.

--- Assets/MTStuff/MTButtonController.cs
+++ Assets/MTStuff/MTButtonController.cs
@@ -5,31 +5,58 @@ using UnityEngine.UI;
 
 public class MTButtonController : MonoBehaviour
 {
-    public Button attackButton;
-    public Button magicButton;
-    public Button fireAttack;
-    public Button iceAttack;
-    public Button thunderAttack;
-    public Button healButton;
-    public Button backButton;
+    Button attackButton;
+    Button magicButton;
+    Button fireAttack;
+    Button iceAttack;
+    Button thunderAttack;
+    Button healButton;
+    Button backButton;
 
 	// Use this for initialization
 	void Start ()
     {
-        attackButton = GameObject.Find("AttackButton").GetComponent<Button>();
-        magicButton = GameObject.Find("MagicButton").GetComponent<Button>();
-        fireAttack = GameObject.Find("FireAttack").GetComponent<Button>();
-        iceAttack = GameObject.Find("IceAttack").GetComponent<Button>();
-        thunderAttack = GameObject.Find("ThunderAttack").GetComponent<Button>();
-        healButton = GameObject.Find("Heal").GetComponent<Button>();
-        backButton = GameObject.Find("Back").GetComponent<Button>();
+        attackButton = transform.Find("AttackButton").GetComponent<Button>();
+        magicButton = transform.Find("MagicButton").GetComponent<Button>();
+        fireAttack = transform.Find("FireAttack").GetComponent<Button>();
+        iceAttack = transform.Find("IceAttack").GetComponent<Button>();
+        thunderAttack = transform.Find("ThunderAttack").GetComponent<Button>();
+        healButton = transform.Find("Heal").GetComponent<Button>();
+        backButton = transform.Find("Back").GetComponent<Button>();
+
+        InitialButtons();
     }
 	
-	void HideButton()
+	void InitialButtons()
     {
+        attackButton.gameObject.SetActive(true);
+        magicButton.gameObject.SetActive(true);
         fireAttack.gameObject.SetActive(false);
         iceAttack.gameObject.SetActive(false);
         thunderAttack.gameObject.SetActive(false);
         healButton.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(false);
+    }
+
+    public void MagicButtons()
+    {
+        attackButton.gameObject.SetActive(false);
+        magicButton.gameObject.SetActive(false);
+        fireAttack.gameObject.SetActive(true);
+        iceAttack.gameObject.SetActive(true);
+        thunderAttack.gameObject.SetActive(true);
+        healButton.gameObject.SetActive(true);
+        backButton.gameObject.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        attackButton.gameObject.SetActive(true);
+        magicButton.gameObject.SetActive(true);
+        fireAttack.gameObject.SetActive(false);
+        iceAttack.gameObject.SetActive(false);
+        thunderAttack.gameObject.SetActive(false);
+        healButton.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(true);
     }
 }
