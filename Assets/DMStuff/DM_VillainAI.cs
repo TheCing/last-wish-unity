3531fd47e17d14cac40d1b67d7596938d391ff2a
matthewtinker22 <38359672+matthewtinker22@users.noreply.github.com>
2019-02-15 16:23:36 -0500

    Button UI

--- Assets/DMStuff/DM_VillainAI.cs
+++ Assets/DMStuff/DM_VillainAI.cs
@@ -1,30 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DM_VillainAI : MonoBehaviour {
-    int enemyHealth;
+public class DM_VillainAI : MonoBehaviour
+{
+    public float enemyHealth = 1000f;
     int enemyAttack;
 
-    int playerHealth;
-    int playerAttack;
-
-    int enemyPosition;
+    Text enemyHealthText;
 
 
-	// Use this for initialization
-	void Start () {
-        //position start is 0
-        //position.(canvasSize, height/2);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //Move toward closest player(s)
-        //if(player location is...){Move toward the player}
-        //else {move to random spot} 
+    //public int playerHealth;
+    //public int playerAttack;
+
+    //public int[] attackType;
+
+
+    public void playerAttacks()
+    {
+
+        //attackType = new int[] { 1, 2, 3, 4, 5 };
+        //dummy playerAttack Method
+        //bool happens = true;
+    }
+    //string enemyPosition[] = [this.gameObject.transform.position.x, this.gameObject.transform.position.y];
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Player player = new Player;
+        enemyHealth = 1000;
+
+        enemyHealthText = GameObject.Find("EnemyHealth").GetComponent<Text>();
+        enemyHealthText.text = "HP: " + enemyHealth;
+    }
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        float enA = Random.Range(1,10);
+
+        if (player.playerAttacks())
+        {
+            enemyHealth = enemyHealth - playerAttack;
+            return;
+        }
+        else if (playerAttacks.playerMagic()) 
+        {
+            //if the player chooses magic then...
+            if (Fire) {
+            //if they choose Fire Magic then...
+              }
+            else if (Ice) {
+                //if they choose Ice Magic then...
+            }
+            else if (Thunder) {
+                //if they choose Thunder Magic then...
+            }
+
+            return; 
+        }
+
 
         //Choose between spell/physical attack
+
         //Animation based on attack type
         //Create random
         //if(Random = 1){spellAttack()}
@@ -33,7 +76,7 @@ public class DM_VillainAI : MonoBehaviour {
         //Glow Bright if hit by character
         //if(player hits){darkGlow()}
         //else{ takeTurn() }
+        
 
-
-    }
+    }*/
 }
